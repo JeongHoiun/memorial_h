558ded9b535b0a32612a78403ed8ae5dfdf4958f
hujeong <hujeong@suresofttech.com>
2024-04-12 16:51:52 +0900

    SQLite 라이브러리 추가
    - sql 간단 사용법과 함께 라이브러리 추가

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import express, { Request, Response } from "express";
+import sqlite3 from "sqlite3";
 
 const app = express();
 const port = 3000;
 
+const db = new sqlite3.Database(
+  "./memorial.db",
+  sqlite3.OPEN_READWRITE,
+  err => {
+    if (err) {
+      console.error(err.message);
+    }
+  },
+);
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+  db.all("SELECT * FROM lorem", (err, rows) => {
+    console.log(err, rows);
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => {
